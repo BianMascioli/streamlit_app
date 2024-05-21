1a0e00c71677d3b182e616080e333c09428fa5a7
BianMascioli <107081521+BianMascioli@users.noreply.github.com>
2024-05-21 01:43:12 -0300

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,20 @@
 import streamlit as st
 from langchain_community import llms  
 from langchain_community.llms import OpenAI
-
+import streamlit as st
+from langchain_community.llms import OpenAI
 
 st.title('App PPS')
 
 # Get OpenAI API key from the user
 openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
-# Initialize session state for storing history
+# Initialize session state for storing history and saved histories
 if 'history' not in st.session_state:
     st.session_state.history = []
+
+if 'saved_histories' not in st.session_state:
+    st.session_state.saved_histories = {}
 
 def generate_response(input_text):
     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
@@ -21,18 +25,40 @@ def generate_response(input_text):
 
 # Form for user input
 with st.form('my_form'):
-    text = st.text_area('Enter text:', 'Enter text')
-    submitted = st.form_submit_button('Submit')
+    text = st.text_area('Enter text:', 'Escriba su pregunta')
+    submitted = st.form_submit_button('Enviar')
     
     if not openai_api_key.startswith('sk-'):
-        st.warning('Please enter your OpenAI API key!', icon='⚠')
+        st.warning('Por favor introduzca su OpenAI API key!', icon='⚠')
     
     if submitted and openai_api_key.startswith('sk-'):
         generate_response(text)
 
+# Section to save the current history
+with st.form('save_form'):
+    history_name = st.text_input('Escriba un nombre para guardar el historial:')
+    save_history = st.form_submit_button('Guardar historial')
+    
+    if save_history:
+        if history_name:
+            st.session_state.saved_histories[history_name] = st.session_state.history.copy()
+            st.session_state.history.clear()
+            st.success(f'Historial guardado como "{history_name}"')
+        else:
+            st.warning('Por favor introduzca un nombre para el historial!', icon='⚠')
+
 # Display question history
 if st.session_state.history:
-    st.write("## Question History")
+    st.write("## Historial Actual")
     for i, entry in enumerate(st.session_state.history, 1):
         st.write(f"### Q{i}: {entry['prompt']}")
         st.write(f"A{i}: {entry['response']}")
+
+# Display saved histories
+if st.session_state.saved_histories:
+    st.write("## Historiales guardados")
+    for name, history in st.session_state.saved_histories.items():
+        st.write(f"### Historial: {name}")
+        for i, entry in enumerate(history, 1):
+            st.write(f"#### Q{i}: {entry['prompt']}")
+            st.write(f"A{i}: {entry['response']}")
